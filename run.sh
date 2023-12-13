98e5c4323f4f4944acbf9202e045daf50259c469
Oliver Faßbender <git@intrepid.de>
2023-12-13 13:32:49 +0100

    changed run.sh

--- run.sh
+++ run.sh
@@ -60,7 +60,7 @@ get_docker_secret PMA_CONTROLHOST
 get_docker_secret PMA_CONTROLUSER
 get_docker_secret PMA_CONTROLPASS
 
-exec "$@"
+#exec "$@"
 mkdir -p /var/nginx/client_body_temp
 chown nobody:nobody /sessions /var/nginx/client_body_temp
 mkdir -p /var/run/php/
@@ -68,6 +68,6 @@ chown nobody:nobody /var/run/php/
 touch /var/log/php-fpm.log
 chown nobody:nobody /var/log/php-fpm.log
 
-if [ "$1" = 'phpmyadmin' ]; then
-    exec supervisord --nodaemon --configuration="/etc/supervisord.conf" --loglevel=info
-fi
+#if [ "$1" = 'phpmyadmin' ]; then
+  exec supervisord --nodaemon --configuration="/etc/supervisord.conf" --loglevel=info
+#fi
