70b9d78620244c433bf1c92aebdc97c22da7c9fd
Oliver Faßbender <git@intrepid.de>
2024-01-16 09:23:06 +0100

    updated Entrypoint run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
-
-    if [ ! -f /etc/phpmyadmin/config.secret.inc.php ]; then
-        cat > /etc/phpmyadmin/config.secret.inc.php <<EOT
+if [ ! -f /etc/phpmyadmin/config.secret.inc.php ]; then
+    cat > /etc/phpmyadmin/config.secret.inc.php <<EOT
 <?php
 \$cfg['blowfish_secret'] = '$(tr -dc 'a-zA-Z0-9~!@#$%^&*_()+}{?></";.,[]=-' < /dev/urandom | fold -w 32 | head -n 1)';
 EOT
-    fi
+fi
 
-    if [ ! -f /etc/phpmyadmin/config.user.inc.php ]; then
-        touch /etc/phpmyadmin/config.user.inc.php
-    fi
+if [ ! -f /etc/phpmyadmin/config.user.inc.php ]; then
+    touch /etc/phpmyadmin/config.user.inc.php
 fi
 
 if [ ! -z "${HIDE_PHP_VERSION}" ]; then
@@ -29,14 +26,6 @@ if [ ! -z "${PMA_USER_CONFIG_BASE64}" ]; then
     echo "${PMA_USER_CONFIG_BASE64}" | base64 -d > /etc/phpmyadmin/config.user.inc.php
 fi
 
-# start: Apache specific settings
-if [ -n "${APACHE_PORT+x}" ]; then
-    echo "Setting apache port to ${APACHE_PORT}."
-    sed -i "/VirtualHost \*:80/c\\<VirtualHost \*:${APACHE_PORT}\>" /etc/apache2/sites-enabled/000-default.conf
-    sed -i "/Listen 80/c\Listen ${APACHE_PORT}" /etc/apache2/ports.conf
-    apachectl configtest
-fi
-# end: Apache specific settings
 
 get_docker_secret() {
     local env_var="${1}"
